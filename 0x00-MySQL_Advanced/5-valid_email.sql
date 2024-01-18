3a5d6598026daf962f2185f2fbe7f470a2a4ddf5
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-01-18 14:37:54 +0200

    Fix task 5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -5,16 +5,8 @@ CREATE TRIGGER `reset_email`
 BEFORE UPDATE
 ON `users` FOR EACH ROW
 BEGIN
-    DECLARE valid_email_new BOOLEAN;
-
     IF NEW.`email` != OLD.`email` THEN
-        IF NEW.`valid_email` = 0 THEN
-            SET valid_email_new = 1;
-        ELSE
-            SET valid_email_new = 0;
-        END IF;
-
-        SET NEW.`valid_email` = valid_email_new;
+        SET NEW.`valid_email` = 0;
     END IF;
 END//
 

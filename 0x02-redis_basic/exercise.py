819287a59b3f338880adbffc1fc85b3cae316093
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-01-24 17:48:20 +0200

    Add task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -18,3 +18,20 @@ class Cache:
         generated_key = str(uuid.uuid4())
         self._redis.set(generated_key, data)
         return generated_key
+
+    def get(self, key: str, fn: Callable) -> str:
+        """Retrieves and type-converts the value of a specific key using fn"""
+        value = self._redis.get(key)
+
+        if fn:
+            return fn(value)
+
+        return value
+
+    def get_str(self, key: str) -> str:
+        """Retrieves and type-converts the value of a specific key to str."""
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """Retrieves and type-converts the value of a specific key to int."""
+        return self.get(key, int)
